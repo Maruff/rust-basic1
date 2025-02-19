c0fb4d8fa2f607a1623e6d72cfd083a4c76e982a
Mohammed Maruff <bmaruff@gmail.com>
2025-02-20 00:01:42 +0530

    Create user.rs

---
+++ src/services/user.rs
@@ -0,0 +1,18 @@
+use diesel::prelude::*;
+use my_user_api::{models::{User, NewUser, UpdateUser}, errors::UserError};
+
+pub fn get_all(conn: &mut PgConnection) -> Result<Vec<User>, diesel::result::Error> {
+    users::table.load::<User>(conn)
+}
+
+// ... other service functions (get_by_id, create, update, delete)
+// These functions contain the database logic.
+// They are called by the handlers.
+
+pub fn create(conn: &mut PgConnection, new_user: &NewUser) -> Result<User, diesel::result::Error> {
+    diesel::insert_into(users::table)
+        .values(new_user)
+        .get_result::<User>(conn)
+}
+
+// ... other service functions (get_by_id, update, delete)
